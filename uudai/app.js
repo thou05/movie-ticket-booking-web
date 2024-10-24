5107a9fca2ec49e42d30948dbbc1f590402efcca
caothithanhxuan <148192574+thanhxuan1202@users.noreply.github.com>
2024-10-24 09:16:55 +0700

    app.js

--- uudai/app.js
+++ uudai/app.js
@@ -54,7 +54,8 @@ var modal1 = document.getElementById("modal1");
 var btn1 = document.getElementById("openModal1");
 var span1 = document.getElementById("closeModal1"); // Sửa lại để sử dụng ID thay vì class
 
-btn1.onclick = function() {
+btn1.onclick = function(event) {
+    event.preventDefault();
   modal1.style.display = "block";
 }
 
@@ -67,7 +68,8 @@ var modal2 = document.getElementById("modal2");
 var btn2 = document.getElementById("openModal2");
 var span2 = document.getElementById("closeModal2"); // Sử dụng ID cho modal 2
 
-btn2.onclick = function() {
+btn2.onclick = function(event) {
+    event.preventDefault();
   modal2.style.display = "block";
 }
 
@@ -77,7 +79,8 @@ span2.onclick = function() {
 var modal3 = document.getElementById("modal3");
 var btn3 = document.getElementById("openModal3");
 var span3 = document.getElementById("closeModal3");
-btn3.onclick = function() {
+btn3.onclick = function(event) {
+    event.preventDefault();
     modal3.style.display = "block";
   }
   
